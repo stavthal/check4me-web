f5ce7b321d7ad3d4919858b47766a8c31557e299
Stavros Thalassinos <stavros8003@gmail.com>
2025-05-29 08:16:10 +0300

    feat: setup stuff

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,12 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { TabsItem } from "@nuxt/ui";
+
+const items = ref<TabsItem[]>([
+  {
+    label: "Client",
+    icon: "i-lucide-user",
+    content: "This is the account content.",
+  },
+  {
+    label: "Checker",
+    icon: "i-lucide-lock",
+    content: "This is the password content.",
+  },
+]);
+</script>
 
 <template>
   <div class="container">
     <div class="left-div">
       <h1>Εγγραφή</h1>
     </div>
-    <div class="right-div">
-      <!-- Tabs for Different types of users -->
+    <div class="right-div bg-primary">
+      <UTabs :items="items" class="tabs" color="primary" />
     </div>
   </div>
 </template>
@@ -19,9 +34,16 @@
   float: left;
 }
 .right-div {
+  display: flex;
   width: 50%;
   height: 100vh;
   background-color: #ffffff;
   float: right;
+  justify-content: center;
+  align-items: center;
+  padding: 8rem;
+}
+
+.tabs {
 }
 </style>
