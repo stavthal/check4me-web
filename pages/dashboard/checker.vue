283bc70400b3b8b8f443b895ea79575cf6b04bc7
Stavros Thalassinos <stavros8003@gmail.com>
2025-06-05 23:15:46 +0300

    feat: hydrate user composable

--- pages/dashboard/checker.vue
+++ pages/dashboard/checker.vue
@@ -1,7 +1,23 @@
+<script setup lang="ts">
+import { useUserStore } from "#imports";
+
+definePageMeta({ middleware: "auth" });
+
+const { user } = useUserStore();
+
+const { loading } = useHydrateUserStore();
+</script>
+
 <template>
   <div>
     <h1>Checker Page</h1>
     <p>This page is for checking the status of the application.</p>
-    <NuxtLink to="/">Go to Home</NuxtLink>
+
+    <div v-if="loading">Loading user...</div>
+    <template v-else>
+      <p>User: {{ user?.full_name || "Guest" }}</p>
+      <p>Role: {{ user?.role || "No role assigned" }}</p>
+      <NuxtLink to="/">Go to Home</NuxtLink>
+    </template>
   </div>
 </template>
